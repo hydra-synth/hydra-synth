616607a970aa96206c4aeda05e24f3d9b15ce4f9
ojack <ojackson@gmail.com>
2019-04-24 15:16:58 -0700

    removed extraneous files

--- src/renderpass-functions.js
+++ src/renderpass-functions.js
@@ -7,7 +7,6 @@
 var glsl = require('glslify')
 
 module.exports = {
-<<<<<<< HEAD
   sharpen: {
     type: 'renderpass',
     inputs: [
@@ -25,29 +24,6 @@ module.exports = {
         type: 'float',
         name: 'mixWithOriginal',
         default: 0.5
-=======
-  _convolution: {
-    type: 'renderpass_util',
-    glsl: `
-      float kernel [9];
-
-      vec4 _convolution (vec2 uv, float[9] _kernel, float kernelWeight) {
-        vec2 st = uv/resolution.xy;
-        vec2 onePixel = vec2(4.0, 4.0) / resolution.xy;
-        //  vec2 onePixel = vec2(1.0, 1.0);
-        vec4 colorSum =
-          texture2D(prevBuffer, st + onePixel * vec2(-1, -1)) * _kernel[0] +
-          texture2D(prevBuffer, st + onePixel * vec2( 0, -1)) * _kernel[1] +
-          texture2D(prevBuffer, st + onePixel * vec2( 1, -1)) * _kernel[2] +
-          texture2D(prevBuffer, st + onePixel * vec2(-1,  0)) * _kernel[3] +
-          texture2D(prevBuffer, st + onePixel * vec2( 0,  0)) * _kernel[4] +
-          texture2D(prevBuffer, st + onePixel * vec2( 1,  0)) * _kernel[5] +
-          texture2D(prevBuffer, st + onePixel * vec2(-1,  1)) * _kernel[6] +
-          texture2D(prevBuffer, st + onePixel * vec2( 0,  1)) * _kernel[7] +
-          texture2D(prevBuffer, st + onePixel * vec2( 1,  1)) * _kernel[8] ;
-        colorSum /= kernelWeight;
-        return colorSum;
->>>>>>> master
       }
     ],
     frag: glsl('./shaders/sharpen.frag')
@@ -69,7 +45,6 @@ module.exports = {
   },
   blur: {
     type: 'renderpass',
-<<<<<<< HEAD
     inputs: [
       {
         type: 'float',
@@ -79,28 +54,6 @@ module.exports = {
         type: 'float',
         name: 'directionY',
         default: 0.0
-=======
-    glsl: `
-      void main () {
-    //    kernel[0] = -0.125; kernel[1] = -0.125; kernel[2] = -0.125;
-      //  kernel[3] = -0.125; kernel[4] = 1.0; kernel[5] = -0.125;
-      //  kernel[6] = -0.125; kernel[7] = -0.125; kernel[8] = -0.125;
-
-// blur
-     kernel[0] = 0.0; kernel[1] = 1.0; kernel[2] = 0.0;
-     kernel[3] = 1.0; kernel[4] = 1.0; kernel[5] = 1.0;
-     kernel[6] = 0.0; kernel[7] = 1.0; kernel[8] = 0.0;
-
-      kernel[0] = 5.0; kernel[1] = -0.0; kernel[2] = -0.0;
-      kernel[3] = 0.0; kernel[4] = 0.0; kernel[5] = 0.0;
-      kernel[6] = -0.0; kernel[7] = -0.0; kernel[8] = -5.0;
-
-        vec4 sum = _convolution( gl_FragCoord.xy, kernel, 10.0);
-        gl_FragColor = clamp(sum , vec4(0.0), vec4(1.0));
-    //   vec2 st = gl_FragCoord.xy/resolution.xy;
-    //    vec4 col = texture2D(prevBuffer, fract(st));
-    //  gl_FragColor = vec4(st, 1.0, 1.0);
->>>>>>> master
       }
     ],
     frag: glsl('./shaders/gaussian.frag')
