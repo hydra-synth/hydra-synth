42cb24851eb97cb4f7fab25a174a8ab5c0858a6e
oscons <54054343+oscons@users.noreply.github.com>
2019-10-18 18:40:09 +0200

    Fix indentation (4 => 2)

--- test/hydra.js
+++ test/hydra.js
@@ -4,16 +4,16 @@ const sinon = require('sinon')
 const {JSDOM} = require('jsdom')
 
 describe ('HydraSynth', () => {
-    let HydraSynth
-    let dom
-    let canvas
+  let HydraSynth
+  let dom
+  let canvas
 
-    afterEach(() => {
-        sinon.restore()
-    })
+  afterEach(() => {
+    sinon.restore()
+  })
 
-    beforeEach(() => {
-        dom = new JSDOM(`<!DOCTYPE html>
+  beforeEach(() => {
+    dom = new JSDOM(`<!DOCTYPE html>
 <html>
     <head>
         <title></title>
@@ -23,101 +23,101 @@ describe ('HydraSynth', () => {
     </body>
 </html>`)
 
-        global.window = dom.window
-        global.document = dom.window.document
-        global.navigator = dom.window.navigator
+    global.window = dom.window
+    global.document = dom.window.document
+    global.navigator = dom.window.navigator
 
-        canvas = dom.window.document.querySelector('#hydra-canvas')
-        canvas.captureStream = sinon.stub().returns(undefined)
-        canvas.getContext = sinon.stub().returns({})
+    canvas = dom.window.document.querySelector('#hydra-canvas')
+    canvas.captureStream = sinon.stub().returns(undefined)
+    canvas.getContext = sinon.stub().returns({})
 
-        global.navigator.mediaDevices = {
-            getUserMedia: () => Promise.reject({name: 'not implemented / ignore'})
-        }
-        global.navigator.getUserMedia = sinon.mock().returns({})
+    global.navigator.mediaDevices = {
+      getUserMedia: () => Promise.reject({name: 'not implemented / ignore'})
+    }
+    global.navigator.getUserMedia = sinon.mock().returns({})
 
-        global.AudioContext = class {
-            constructor () {
-            }
-            createMediaStreamSource () {
-                return undefined
-            }
-        }
+    global.AudioContext = class {
+      constructor () {
+      }
+      createMediaStreamSource () {
+        return undefined
+      }
+    }
 
-        global.MediaSource = class {
-            constructor () {
-            }
-            addEventListener () {
-                return undefined
-            }
-        }
+    global.MediaSource = class {
+      constructor () {
+      }
+      addEventListener () {
+        return undefined
+      }
+    }
 
-        const cestub = sinon.stub(dom.window.document, 'createElement')
+    const cestub = sinon.stub(dom.window.document, 'createElement')
 
-        cestub.withArgs('canvas').callsFake(
-            function (...args) {
-                const ret = cestub.wrappedMethod.apply(this, args)
+    cestub.withArgs('canvas').callsFake(
+      function (...args) {
+        const ret = cestub.wrappedMethod.apply(this, args)
 
-                ret.getContext = sinon.stub().returns({})
+        ret.getContext = sinon.stub().returns({})
 
-                return ret
-            }
-        )
-        cestub.callThrough()
+        return ret
+      }
+    )
+    cestub.callThrough()
 
-        HydraSynth = require('../index')
+    HydraSynth = require('../index')
 
-        HydraSynth.prototype._initRegl = function () {
-            this.regl = {
-                buffer: () => {},
-                prop: () => {},
-                texture: () => {},
-                framebuffer: () => {}
-            },
-            this.renderFbo = () => {},
-            this.renderAll = () => {}
-        }
+    HydraSynth.prototype._initRegl = function () {
+      this.regl = {
+        buffer: () => {},
+        prop: () => {},
+        texture: () => {},
+        framebuffer: () => {}
+      },
+      this.renderFbo = () => {},
+      this.renderAll = () => {}
+    }
 
+  })
+
+  it ('Sets up basic infrastructure', () => {
+    const hydra = new HydraSynth({autoLoop: false, makeGlobal: false, canvas})
+
+    expect(hydra)
+      .to.be.an('object')
+      .and.to.include.keys(['s', 'a', 'audio', 'bpm'])
+
+    expect(hydra.bpm)
+      .to.be.a('number')
+      .and.to.be.equal(60)
+  })
+
+  describe ('makeGlobal', () => {
+    it('Does not create global variables if set to false', () => {
+      const prev_keys = Object.keys(global.window)
+
+      const hydra = new HydraSynth({autoLoop: false, makeGlobal: false, canvas})
+
+      const new_keys = Object.keys(global.window).filter(x => prev_keys.indexOf(x) < 0)
+
+      expect(new_keys).to.have.lengthOf(0)
     })
 
-    it ('Sets up basic infrastructure', () => {
-        const hydra = new HydraSynth({autoLoop: false, makeGlobal: false, canvas})
+    it('Creates expected global variables if set to true', () => {
+      const transforms = require('../src/glsl/composable-glsl-functions')
 
-        expect(hydra)
-            .to.be.an('object')
-            .and.to.include.keys(['s', 'a', 'audio', 'bpm'])
+      const prev_keys = Object.keys(global.window)
 
-        expect(hydra.bpm)
-            .to.be.a('number')
-            .and.to.be.equal(60)
+      const hydra = new HydraSynth({autoLoop: false, makeGlobal: true, canvas})
+
+      const new_keys = Object.keys(global.window).filter(x => prev_keys.indexOf(x) < 0)
+
+      expect(new_keys).to.be.an('array').and.to.include.members([
+        ...Object.entries(transforms).filter(([, transform]) => transform.type === 'src').map(([name]) => name),
+        ...Array(hydra.s.length).fill(1).map((_, i) => `s${i}`),
+        ...Array(hydra.audio.bins.length).fill(1).map((_, i) => `a${i}`),
+        'bpm', 'mouse', 'time', 'a', 'synth', 'render', 'screencap', 'vidRecorder'
+      ])
     })
-
-    describe ('makeGlobal', () => {
-        it('Does not create global variables if set to false', () => {
-            const prev_keys = Object.keys(global.window)
-
-            const hydra = new HydraSynth({autoLoop: false, makeGlobal: false, canvas})
-
-            const new_keys = Object.keys(global.window).filter(x => prev_keys.indexOf(x) < 0)
-
-            expect(new_keys).to.have.lengthOf(0)
-        })
-
-        it('Creates expected global variables if set to true', () => {
-            const transforms = require('../src/glsl/composable-glsl-functions')
-
-            const prev_keys = Object.keys(global.window)
-
-            const hydra = new HydraSynth({autoLoop: false, makeGlobal: true, canvas})
-
-            const new_keys = Object.keys(global.window).filter(x => prev_keys.indexOf(x) < 0)
-
-            expect(new_keys).to.be.an('array').and.to.include.members([
-                ...Object.entries(transforms).filter(([, transform]) => transform.type === 'src').map(([name]) => name),
-                ...Array(hydra.s.length).fill(1).map((_, i) => `s${i}`),
-                ...Array(hydra.audio.bins.length).fill(1).map((_, i) => `a${i}`),
-                'bpm', 'mouse', 'time', 'a', 'synth', 'render', 'screencap', 'vidRecorder'
-            ])
-        })
-    })
+  })
 })
--- test/synth.js
+++ test/synth.js
@@ -7,95 +7,95 @@ const {JSDOM} = require('jsdom')
 const Synth = require('../src/create-synth')
 
 describe('Synth', () => {
-    let dummyOutput
+  let dummyOutput
 
-    beforeEach(() => {
-        dummyOutput = {}
+  beforeEach(() => {
+    dummyOutput = {}
+  })
+
+  it('Sets the seq prototype on Array', () => {
+    expect(Array.prototype).to.include.keys('fast')
+
+    expect(Array.prototype.fast).to.be.a('function')
+  })
+
+  it('Contains all transforms', () => {
+    const transforms = require('../src/glsl/composable-glsl-functions')
+    const srcNames = Object.entries(transforms).filter(([, transform]) => transform.type === 'src').map(([name]) => name)
+
+    const events = []
+    const synth = new Synth(dummyOutput, {}, (e) => events.push(e))
+
+    expect(synth.generators)
+      .to.be.an('object')
+      .and.to.have.all.keys(srcNames)
+
+    expect(events.filter(({type}) => type === 'add').map(({method}) => method))
+      .to.have.members(srcNames)
+  })
+
+  it('Can be extended', () => {
+    const transforms = require('../src/glsl/composable-glsl-functions')
+    const srcNames = Object.entries(transforms).filter(([, transform]) => transform.type === 'src').map(([name]) => name)
+
+    const events = []
+    const synth = new Synth(dummyOutput, 'invalid', (e) => events.push(e))
+
+    expect(synth.generators)
+      .to.be.an('object')
+      .and.to.have.all.keys(srcNames)
+
+    expect(events.filter(({type}) => type === 'add').map(({method}) => method))
+      .to.include.members(srcNames);
+
+    [{
+      foo: {
+        type: 'src',
+        inputs: [],
+        glsl: '<foo>'
+      }
+    },
+    [
+      {
+        name: 'foo',
+        type: 'src',
+        inputs: [],
+        glsl: '<foo>'
+      }
+    ]].forEach((ext, i) => {
+      synth.extendTransforms = ext
+
+      events.length = 0
+      synth.init()
+
+      expect(events.filter(({type}) => type === 'remove').map(({method}) => method))
+        .to.include.all.members(srcNames)
+
+      expect(events.filter(({type}) => type === 'add').map(({method}) => method))
+        .to.have.all.members([
+          ...srcNames,
+          'foo'
+        ])
     })
 
-    it('Sets the seq prototype on Array', () => {
-        expect(Array.prototype).to.include.keys('fast')
-
-        expect(Array.prototype.fast).to.be.a('function')
+    synth.setFunction('bar', {
+      type: 'src',
+      inputs: [],
+      glsl: '<bar>'
     })
 
-    it('Contains all transforms', () => {
-        const transforms = require('../src/glsl/composable-glsl-functions')
-        const srcNames = Object.entries(transforms).filter(([, transform]) => transform.type === 'src').map(([name]) => name)
+    expect(synth.generators).to.include.keys('bar')
 
-        const events = []
-        const synth = new Synth(dummyOutput, {}, (e) => events.push(e))
 
-        expect(synth.generators)
-            .to.be.an('object')
-            .and.to.have.all.keys(srcNames)
 
-        expect(events.filter(({type}) => type === 'add').map(({method}) => method))
-            .to.have.members(srcNames)
+  })
+
+  it('Can create function chains', () => {
+    const synth = new Synth(dummyOutput)
+
+    assert.doesNotThrow(() => {
+      synth.generators.solid().repeatX()
     })
-
-    it('Can be extended', () => {
-        const transforms = require('../src/glsl/composable-glsl-functions')
-        const srcNames = Object.entries(transforms).filter(([, transform]) => transform.type === 'src').map(([name]) => name)
-
-        const events = []
-        const synth = new Synth(dummyOutput, 'invalid', (e) => events.push(e))
-
-        expect(synth.generators)
-            .to.be.an('object')
-            .and.to.have.all.keys(srcNames)
-
-        expect(events.filter(({type}) => type === 'add').map(({method}) => method))
-            .to.include.members(srcNames);
-
-        [{
-            foo: {
-                type: 'src',
-                inputs: [],
-                glsl: '<foo>'
-            }
-        },
-        [
-            {
-                name: 'foo',
-                type: 'src',
-                inputs: [],
-                glsl: '<foo>'
-            }
-        ]].forEach((ext, i) => {
-            synth.extendTransforms = ext
-
-            events.length = 0
-            synth.init()
-
-            expect(events.filter(({type}) => type === 'remove').map(({method}) => method))
-                .to.include.all.members(srcNames)
-
-            expect(events.filter(({type}) => type === 'add').map(({method}) => method))
-                .to.have.all.members([
-                    ...srcNames,
-                    'foo'
-                ])
-        })
-
-        synth.setFunction('bar', {
-            type: 'src',
-            inputs: [],
-            glsl: '<bar>'
-        })
-
-        expect(synth.generators).to.include.keys('bar')
-
-
-
-    })
-
-    it('Can create function chains', () => {
-        const synth = new Synth(dummyOutput)
-
-        assert.doesNotThrow(() => {
-            synth.generators.solid().repeatX()
-        })
-    })
+  })
 })
 
